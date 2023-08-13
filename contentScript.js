a53abb25fae80a058f3a9e2e42dfdf77a96c571c
unknown <chandrachurmukherjee96@gmail.com>
2023-08-13 16:30:30 +0530

    Fixed the code to work with other score formats

--- contentScript.js
+++ contentScript.js
@@ -1,4 +1,4 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
   if (request.action === 'sortComparison') {
     let meanDifference = 0;
 
@@ -13,22 +13,24 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       allEntries.forEach(entry => {
         const yourStatus = entry.querySelector('.status:nth-child(4)').textContent.trim();
         const theirStatus = entry.querySelector('.status:nth-child(5)').textContent.trim();
-        const yourScore = parseFloat(entry.querySelector('.score:nth-child(2)').textContent.trim());
-        const theirScore = parseFloat(entry.querySelector('.score:nth-child(3)').textContent.trim());
+        const yourScore = parseFloat(entry.querySelector('.score:nth-child(2)').getAttribute('score'));
+        const theirScore = (parseFloat(entry.querySelector('.score:nth-child(3)').getAttribute('score')))*request.scoreScaleFactor;
 
         if (yourScore === 0 || theirScore === 0 || yourStatus === 'Planning' || theirStatus === 'Planning') {
           entry.remove();
         } else {
-          sumOfDifferences += Math.abs(yourScore - theirScore);
+          sumOfDifferences += (yourScore - theirScore) * (yourScore - theirScore);
           consideredEntriesCount++;
         }
       });
 
-      meanDifference = (sumOfDifferences / consideredEntriesCount).toFixed(2);
+      meanDifference = (Math.sqrt(sumOfDifferences / consideredEntriesCount)).toFixed(2);
 
       const sortedEntries = allEntries.filter(entry => entry.parentElement === compareElement).sort((a, b) => {
-        const aScoreDifference = Math.abs(parseFloat(a.querySelector('.score:nth-child(2)').textContent) - parseFloat(a.querySelector('.score:nth-child(3)').textContent));
-        const bScoreDifference = Math.abs(parseFloat(b.querySelector('.score:nth-child(2)').textContent) - parseFloat(b.querySelector('.score:nth-child(3)').textContent));
+        const aScoreDifference = Math.abs(parseFloat(a.querySelector('.score:nth-child(2)').getAttribute('score')) - 
+                                  (parseFloat(a.querySelector('.score:nth-child(3)').getAttribute('score')))*request.scoreScaleFactor);
+        const bScoreDifference = Math.abs(parseFloat(b.querySelector('.score:nth-child(2)').getAttribute('score')) - 
+                                  (parseFloat(b.querySelector('.score:nth-child(3)').getAttribute('score')))*request.scoreScaleFactor);
         return aScoreDifference - bScoreDifference;
       });
 
@@ -40,3 +42,4 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     }
   }
 });
+
--- popup.js
+++ popup.js
@@ -8,14 +8,52 @@ document.addEventListener('DOMContentLoaded', () => {
 
   sortButton.addEventListener('click', () => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      const url = tabs[0].url;
-      if (url.includes('anilist.co/user/') && url.includes('/compare')) {
-        chrome.tabs.sendMessage(tabs[0].id, { action: 'sortComparison' }, (response) => {
+      fetchUserPrecision(tabs[0].url.match(/\/user\/([^/]+)\//)[1]).then(scoreScaleFactor => {
+        chrome.tabs.sendMessage(tabs[0].id, { action: 'sortComparison', scoreScaleFactor: scoreScaleFactor }, (response) => {
           console.log(response);
           const meanScoreElement = document.querySelector('.mean-score');
-          meanScoreElement.innerHTML = `Mean difference: ${response.meanDifference}`;
+          meanScoreElement.innerHTML = `RMS difference: ${response.meanDifference}`;
         });
-      }
+      })
     });
   });
 });
+
+async function fetchUserPrecision(username) {
+  const query = `
+    query ($name: String) {
+      User(name:$name) {
+        mediaListOptions{
+          scoreFormat
+        }
+      }
+    }
+  `;
+
+  const variables = {"name": username};
+  
+  const response = await fetch('https://graphql.anilist.co', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    },
+    body: JSON.stringify({
+      query: query,
+      variables: variables,
+    }),
+  })
+  const json = await response.json();
+  switch(json.data.User.mediaListOptions.scoreFormat) {
+    case "POINT_100":
+      return 0.1;
+    case "POINT_10_DECIMAL":
+      return 1;
+    case "POINT_10":
+      return 1;
+    case "POINT_5":
+      return 2;
+    case "POINT_3":
+      return 3;
+  }
+}
